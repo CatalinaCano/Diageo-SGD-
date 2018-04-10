39d8d25934f59c7b8ad8f5c8b32004eb9dc74ed8
Catalina Cano <catalinacano08@hotmail.com>
2018-04-10 18:06:01 -0500

    Creacion del registro del producto

--- src/app/pages/register-product/register-product.component.ts
+++ src/app/pages/register-product/register-product.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register-product',
-  templateUrl: './register-product.component.html'
+  templateUrl: './register-product.component.html',
+  styleUrls: ['./register-product.component.css']
 })
 export class RegisterProductComponent implements OnInit {
 
